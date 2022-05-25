e04bcf9dc69d8985a3ace010a32f68b744a6ceda
smitgol <60008621+smitgol@users.noreply.github.com>
2022-05-25 15:38:07 +0530

    pure and view function

--- functions_variable.sol
+++ functions_variable.sol
@@ -7,10 +7,17 @@ contract local  //declaring contract
     uint age=10; // state varaible 
     uint public height=21 // public varaible
 
-    function getter() public view returns(uint)
+    function getter() public view returns(uint) //view can only read state variable in function
     {
         return age;
     }
+    
+    function get_age() public pure returns(unit) //pure cannot read state and write state variable
+    {
+        uint age = 15; // local variable
+        return age;
+    }
+    
     function setter(uint newage) public 
     {
         age=newage;
